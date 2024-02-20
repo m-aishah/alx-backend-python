f1e053f75558c8fae545ffcd6d7f16ea3fd79c81
Aishah <maishah2540@gmail.com>
2024-02-20 19:05:33 +0000

    Task 2.

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Contains measure_runtime coroutine.'''
+import asyncio
+import time
+from importlib import import_module as using
+
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''Runs async_comprehension 4 times and calculates the runtime.'''
+    start_time = time.time()
+    await asyncio.gather(
+        *(async_comprehension() for _ in range(4))
+    )
+
+    return time.time() - start_time
